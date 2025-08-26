1d279a1ef4a8ef41002160fadede1425e09ebeae
SilenceRichard <silencerichard@163.com>
2025-08-26 17:28:07 +0800

    明确配置vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    copyPublicDir: true,
+    rollupOptions: {
+      // 确保 _redirects 文件被包含
+      external: [],
+    }
+  },
+  publicDir: 'public'
 })
